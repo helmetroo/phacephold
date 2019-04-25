8f6b4810654d8860d51f0aefef498afb2b93b6b3
Samuel Sweet <samueljsweet@gmail.com>
2019-04-25 16:31:01 -0600

    Cached FPS counter element in constructor

--- src/fps-counter.ts
+++ src/fps-counter.ts
@@ -1,20 +1,23 @@
 export default class FPSCounter {
     private static readonly ELEMENT_ID = '#fps';
+    private fpsElement: Element | null = null;
 
     private lastUpdate: number = 0;
 
+    constructor() {
+        this.fpsElement =
+            document.querySelector(FPSCounter.ELEMENT_ID);
+    }
+
     public update() {
+        if(!this.fpsElement)
+            return;
+
         const now = window.performance.now();
         const interval = now - this.lastUpdate;
         this.lastUpdate = now;
 
         const fps = Math.round(1000 / interval);
-
-        const fpsElement =
-            document.querySelector(FPSCounter.ELEMENT_ID);
-        if(!fpsElement)
-            return;
-
-        fpsElement.textContent = `${fps} fps`;
+        this.fpsElement.textContent = `${fps} fps`;
     }
 }
